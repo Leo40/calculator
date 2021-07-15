fc6825bc70cab1cf6afdd0527d7f5b5c6e595691
Mustafa <myasad91@gmail.com>
2021-07-15 18:47:33 +0300

    Implemented header section; refined other sections.

---
+++ calculator/src/components/Header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div className="Header">
+            <div className="Left-Section">
+                Calc
+            </div>
+            <div className="Right-Section"> 
+                <div>
+                    THEME
+                </div>
+                <div className="Toggle">
+                    <div>
+                        1 2 3
+                    </div>
+                    <div>
+                        <div className="Outer">
+                            <div className="Inner"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Header
